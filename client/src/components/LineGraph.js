3e85f9aa887c92f4021a77b2b5425d993de18025
devsoor <soor.dev@gmail.com>
2020-03-27 11:05:26 -0700

    log vs linear radio button

--- client/src/components/LineGraph.js
+++ client/src/components/LineGraph.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import {Card, CardBody, Col} from 'reactstrap';
+import React, {useState} from 'react';
+import {Row, Col, Card, CardBody, Button, ButtonGroup} from 'reactstrap';
 import {Line} from 'react-chartjs-2';
 import 'chartjs-plugin-datalabels';
 
 const LineGraph = (props) => {
+    const [gtype, setGtype] = useState("linear");
+
     const {statesDailyData, statesCoords} = props;
     const randomColorGenerator = () => { 
       return '#' + (Math.random().toString(16) + '0000000').slice(4, 8); 
@@ -12,7 +14,6 @@ const LineGraph = (props) => {
     const confirmedCases = () => {
         const data = {
           labels: [],
-        //   backgroundColor: randomColorGenerator(),
           datasets: []
       } 
 
@@ -52,56 +53,68 @@ const LineGraph = (props) => {
     }
 
     return <Col>
-          <Card>
-        <CardBody>
-        <h3>Test results over past 3 weeks</h3>
-        <Line data={confirmedCases}
-        options={{
-            plugins: {
-                labels: false,
-                clamp: true,
-                datalabels: {
-                    display: false,
-                    align: 'end',
-                    fontColor: 'red',
-                    anchor: 'end',
-                    // rotation: 270,
-                    formatter: function(value, context) {
-                            return (value.toLocaleString());
-                    },
-                },
-                borderRadius: 4,
-                color: 'white',
-                font: {
-                    weight: 'bold'
-                },
-            },
-            maintainAspectRatio: true,
-            'scales': {
-                'xAxes': [
-                    {
-                        'gridLines': { 'display': false },
-                        'ticks': { 'fontFamily': 'Poppins', autoSkip: false },
-            
-                    }
-                ],
-                'yAxes': [
-                    {
-                        'type': "logarithmic",
-                        'gridLines': { 'display': true },
-                        'ticks': { 
-                            'fontFamily': 'Poppins',
-                            callback: function(value, index, values) {
-                                return (value.toLocaleString());
-                            }
-                        }
-                    }
-                ]
-            },
-        }}
-        />
-        </CardBody>
-      </Card>
+                <Card>
+                    <CardBody>
+                        <Row className="p-4">
+                                <Col sm={8}>
+                                    <h3>Test results over past 3 weeks</h3>
+                                </Col>
+                                <Col sm={4}>
+                                <ButtonGroup className="pull-right" >
+                                    <Button className="bg-warning text-white" outline style={{border:1}} onClick={()=> setGtype("linear")}>Linear</Button>
+                                    <Button  className="bg-danger text-white" outline style={{border:1}} onClick={()=>setGtype("logarithmic")}>Logarithmic</Button>
+                                </ButtonGroup>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Line data={confirmedCases}
+                            options={{
+                                plugins: {
+                                    labels: false,
+                                    clamp: true,
+                                    datalabels: {
+                                        display: false,
+                                        align: 'end',
+                                        fontColor: 'red',
+                                        anchor: 'end',
+                                        // rotation: 270,
+                                        formatter: function(value, context) {
+                                                return (value.toLocaleString());
+                                        },
+                                    },
+                                    borderRadius: 4,
+                                    color: 'white',
+                                    font: {
+                                        weight: 'bold'
+                                    },
+                                },
+                                maintainAspectRatio: true,
+                                'scales': {
+                                    'xAxes': [
+                                        {
+                                            'gridLines': { 'display': false },
+                                            'ticks': { 'fontFamily': 'Poppins', autoSkip: false },
+                                
+                                        }
+                                    ],
+                                    'yAxes': [
+                                        {
+                                            'type':`${gtype}`,
+                                            'gridLines': { 'display': true },
+                                            'ticks': { 
+                                                'fontFamily': 'Poppins',
+                                                callback: function(value, index, values) {
+                                                    return (value.toLocaleString());
+                                                }
+                                            }
+                                        }
+                                    ]
+                                },
+                            }}
+                            />
+                        </Row>
+                    </CardBody>
+        </Card>
     </Col>
 }
 
